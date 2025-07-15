fbd838ee384439b6c4bb78a9e077b591601aa3d4
Sebastian Karasiewicz <sekarasiewicz@gmail.com>
2025-07-15 12:37:16 +0200

    Initialize card deck with suits and ranks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+    let ranks = [
+        "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace",
+    ];
+
+    for suit in suits {
+        for rank in ranks {
+            let card = format!("{} of {}", rank, suit);
+            println!("{}", card);
+        }
+    }
+
+    let deck = Deck { cards: vec![] };
+
+    println!("Here is your deck: {:?}", deck);
 }
