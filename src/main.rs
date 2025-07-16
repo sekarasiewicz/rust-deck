fbca4cbf11fa54861b4f14622e03a76d49bed876
Sebastian Karasiewicz <sekarasiewicz@gmail.com>
2025-07-16 15:39:25 +0200

    Move deck creation logic into Deck::new constructor

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,30 @@ struct Deck {
     cards: Vec<String>,
 }
 
-fn main() {
-    let suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
-    let ranks = [
-        "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace",
-    ];
+impl Deck {
+    fn new() -> Self {
+        let suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+        let ranks = [
+            "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace",
+        ];
 
-    let mut cards = vec![];
+        let mut cards = vec![];
 
-    for suit in suits {
-        for rank in ranks {
-            let card = format!("{} of {}", rank, suit);
-            cards.push(card);
+        for suit in suits {
+            for rank in ranks {
+                let card = format!("{} of {}", rank, suit);
+                cards.push(card);
+            }
         }
-    }
 
-    let deck = Deck { cards };
+        let deck = Deck { cards };
+
+        return deck;
+    }
+}
+
+fn main() {
+    let deck = Deck::new();
 
     println!("Here is your deck: {:#?}", deck);
 }
